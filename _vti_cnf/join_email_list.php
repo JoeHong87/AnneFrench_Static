006288f954eff52b531b325c1d946b5b736c9d9f
JoeHong87 <joehong@gmail.com>
2014-12-04 14:58:30 -0500

    Fixing header for Modern & Contemporary

--- _vti_cnf/join_email_list.php
+++ _vti_cnf/join_email_list.php
@@ -1,7 +1,7 @@
 vti_encoding:SR|utf8-nl
 vti_timelastmodified:TR|24 Mar 2012 20:49:00 -0000
 vti_extenderversion:SR|6.0.2.8161
-vti_backlinkinfo:VX|Index-9.htm Index-9s3.htm Index-9s4.htm join_email_list2.htm FAQ.htm Artists2.htm Artists.htm Index-9s.htm About.htm
+vti_backlinkinfo:VX|Index-9.htm Index-9s3.htm Index-9s4.htm join_email_list2.htm FAQ.htm Artists2.htm Artists.htm Artists3.htm Index-9s.htm About.htm
 vti_author:SR|MY-DELL\\John Hong
 vti_modifiedby:SR|JohnHong-PC\\John Hong
 vti_timecreated:TR|07 Aug 2010 11:06:00 -0000
